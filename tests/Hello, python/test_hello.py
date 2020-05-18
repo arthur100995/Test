ea5338e3e18176099c24310e0f9ffd8d05133e85
arthur100995 <arthur100995@gmail.com>
2020-05-18 19:24:03 +0300

    update

--- tests/Hello, python/test_hello.py
+++ tests/Hello, python/test_hello.py
@@ -1,2 +1,32 @@
-print("You've successfully run some Python code")
-print("Congratulations!")
+#1 Complete the code below
+# pi = 3.14159
+# diameter = 3
+#
+# # Create a variable called 'radius' equal to half the diameter
+# radius = diameter / 2
+#
+# # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+# area = pi * (radius ** 2)
+
+#2 Add code to the following cell to swap variables a and b (so that a refers to the object previously referred to by b and vice versa).
+# a = [1, 2, 3]
+# b = [3, 2, 1]
+# a = b
+# b = a
+
+# a) Add parentheses to the following expression so that it evaluates to 1.
+# (5 - 3) // 2
+# b) Add parentheses to the following expression so that it evaluates to 0
+# 8 - (3 * 2) - (1 + 1)
+
+
+
+
+
+# Variables representing the number of candies collected by alice, bob, and carol
+# alice_candies = 121
+# bob_candies = 77
+# carol_candies = 109
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+# to_smash = (alice_candies + bob_candies + carol_candies) % 3
