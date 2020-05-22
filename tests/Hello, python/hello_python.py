cca0c0e45af7d4a53998787008e028428779b8d2
arthur100995 <arthur100995@gmail.com>
2020-05-22 10:07:23 +0300

    update

--- tests/Hello, python/hello_python.py
+++ tests/Hello, python/hello_python.py
@@ -1,2 +1,3 @@
 from test_for_hello_python import *
 
+print(area_circle())
--- tests/Hello, python/test_for_hello_python.py
+++ tests/Hello, python/test_for_hello_python.py
@@ -1,22 +1,22 @@
 from hello_python import *
 
-#TEST FOR ARITHMETIC OPERATIONS
-def addition(a, b):
-    return a + b
-def subtraction(a, b):
-    return a - b
-def multiplycation(a, b):
-    return a * b
-def true_division(a, b):
-    return a / b
-def floor_division(a, b):
-    return a // b
-def modulus(a, b):
-    return a % b
-def exponentiation(a, b):
-    return a ** b
-def negation(a):
-    return - a
+# #TEST FOR ARITHMETIC OPERATIONS
+# def addition(a, b):
+#     return a + b
+# def subtraction(a, b):
+#     return a - b
+# def multiplycation(a, b):
+#     return a * b
+# def true_division(a, b):
+#     return a / b
+# def floor_division(a, b):
+#     return a // b
+# def modulus(a, b):
+#     return a % b
+# def exponentiation(a, b):
+#     return a ** b
+# def negation(a):
+#     return - a
 
 # TEST FOR DEFINING AREA OF CIRCLE
 def area_circle():
@@ -27,17 +27,16 @@ def area_circle():
     area = pi * (radius **2)
     return area
 
-# TEST FOR SWAP VARIABLE
-def swap_variable(a, b):
-    a, b = b, a
-    return a, b
-
-#FOR THE SAKE OF THEIR FRIENDSHIP
-def candies_to_smash():
-    alice_candies, bob_candies, carol_candies = int(input()), int(input()), int(input())
-    to_smash = (alice_candies + bob_candies + carol_candies) % 3
-    return to_smash
-
+# # TEST FOR SWAP VARIABLE
+# def swap_variable(a, b):
+#     a, b = b, a
+#     return a, b
+#
+# #FOR THE SAKE OF THEIR FRIENDSHIP
+# def candies_to_smash():
+#     alice_candies, bob_candies, carol_candies = int(input()), int(input()), int(input())
+#     to_smash = (alice_candies + bob_candies + carol_candies) % 3
+#     return to_smash
 
 
 
