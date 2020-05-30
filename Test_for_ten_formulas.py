0faa56b4b3410f2d94450a2449de2a300775154d
arthur100995 <arthur100995@gmail.com>
2020-05-30 17:36:28 +0300

    Test for ten formulas of Geometry. (Second version)

--- Test_for_ten_formulas.py
+++ Test_for_ten_formulas.py
@@ -3,40 +3,54 @@ from math import *
 
 class TestTenGeometricFormulas(unittest.TestCase):
 
-    def setUp(self):
-        self.area = float
-        self.perimeter = float
-
     def test_area_square(self):
         '''area_square(площадь квадрата) = a(сторона) ** 2'''
-        self.assertEqual(self.area(9), 3 ** 2)
+        a = int()
+        area_square = a ** 2
+        self.assertEqual(area_square == 9, a == 3)
 
     def test_area_triangle(self):
         '''area_triangle(площадь треугольника) = (a(основание) * h(высота)) / 2'''
-        self.assertEqual(self.area(2), (2 * 2) / 2)
+        a = int()
+        h = int()
+        area_triangle = (a * h) / 2
+        self.assertEqual(area_triangle == 3, a == 2, h == 3)
 
     def test_area_rectangle(self):
         '''area_rectangle(площадь прямоугольника) = a(сторона1) * b(сторона 2)'''
-        self.assertEqual(self.area(6), 2 * 3)
+        a = int()
+        b = int()
+        area_rectangle = a * b
+        self.assertEqual(area_rectangle == 15, a == 3, b == 5)
 
     def test_area_round(self):
         '''area_round(площадь круга) = pi * (r(радиус)**2)'''
-        self.assertEqual(self.area(pi * 4), pi * (2 ** 2))
+        r = int()
+        area_round = pi * (r**2)
+        self.assertEqual(area_round == (pi * 9), r == 3)
 
     def test_area_righttriangle(self):
         '''area_righttriangle(площадь прямоугольного треугольника) = (a * b)(умножение катетов) / 2'''
-        self.assertEqual(self.area(3), (3 * 2) / 2)
+        a = int()
+        b = int()
+        area_righttriangle = (a * b) / 2
+        self.assertEqual(area_righttriangle == 5, a == 2, b == 5)
 
     def test_area_isoscelestriangle(self):
         '''area_isosceles triangle(площадь ровнобедренного треугольника) = 1/2 * b(основание) * h(высота)'''
-        self.assertEqual(self.area(3), 0.5 * 2 * 3)
+        b = int()
+        h = int()
+        area_isosceles_triangle = 0.5 * b * h
+        self.assertEqual(area_isosceles_triangle == 8, b == 4, h == 4)
 
     def test_perimeter_round(self):
         '''
         perimeter of round = pi(число Пи) * D(диаметр окружности),
         или = 2 * pi * r(радиус)
         '''
-        self.assertEqual(self.perimeter(pi * 12), 2 * pi * 6)
+        D = int()
+        perimeter_round = pi * D
+        self.assertEqual(perimeter_round == pi * 12, D == 12)
 
 
 
