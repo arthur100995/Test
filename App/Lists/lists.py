6b2f8fbab035f78df27785f536a45bc37f9fec1d
arthur100995 <arthur100995@gmail.com>
2020-06-05 11:18:41 +0300

    Completed all tests for lists.

--- App/Lists/lists.py
+++ App/Lists/lists.py
@@ -10,3 +10,23 @@ def losing_team_captain(teams):
     """Given a list of teams, where each team is a list of names,
      return the 2nd player (captain) from the last listed team"""
     return teams[-1][1]
+
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list)
+     to last place and vice versa."""
+    tmp = racers[0]
+    racers[0] = racers[-1]
+    racers[-1] = tmp
+    return racers
+
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name,
+     return whether the guest with that name was fashionably late."""
+    if arrivals.index(name) > len(arrivals) // 2:
+        if arrivals.index(name) < len(arrivals) - 1:
+            return True
+        else:
+            return False
+    else:
+        return False
+
--- tests/Lists/tests_lists.py
+++ tests/Lists/tests_lists.py
@@ -16,6 +16,17 @@ class ListTests(unittest.TestCase):
         """Test for function 'losing_team_captain'"""
         self.assertEqual(losing_team_captain([[1], [2], [0, 7]]), 7)
 
+    def test_purple_shell(self):
+        """Test for function 'purple_shell'"""
+        self.assertEqual(purple_shell([0, 1, 2, 3]), [3, 1, 2, 0])
+
+
+    def test_fashionably_late(self):
+        """Test for function 'fashionably_late'"""
+        self.assertTrue(fashionably_late([1, 2, 3, 4, 5], 4))
+        self.assertTrue(fashionably_late(['a', 'b', 'c', 'd', 'e', 'f'], 'e'))
+        self.assertIs(fashionably_late(['a', 'b', 'c', 'd', 'e', 'f'], 'e'), True)
+
 
 
 
